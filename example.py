1cd51de8b007052f33927874593e4e00dab84a6a
cph101 <cph101@icloud.com>
2024-10-01 16:41:21 +0200

    Init
    Signed-off-by: cph101 <cph101@icloud.com>

---
+++ example.py
@@ -0,0 +1,5 @@
+from src.multichoicelib import MultiChoice
+
+query = MultiChoice("Pick one:", ["apples", "cherries"]).wAccentColor("\033[33m").wQuestionPrefix("\033[4m").wAnswerPrefix("] ", True).wSelectedPrefix(") ", True).addOption("oranges")()
+
+print(query.index, query.value)
